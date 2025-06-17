7e69c014ea8bf484c14ed7fd477a4164f5361973
PortableL <litoappweb@gmail.com>
2025-06-17 16:06:24 +0800

    feat:with install flask and flask requirements

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def cashin():
     return render_template('CashIn.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
